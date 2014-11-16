3695ae03b962e8b9bc8ea532078b4c8c3039da1e
maniart <mani.art@gmail.com>
2014-11-15 19:40:47 -0500

    added morgan and debug

--- app.js
+++ app.js
@@ -4,28 +4,79 @@ express,
 path,
 socketio,
 app,
+logger,
+bodyParser,
+debug,
 server,
 port,
 io;
-
 
 http = require('http');
 express = require('express');
 path = require('path');
 socketio = require('socket.io');
+logger = require('morgan');
+bodyParser = require('body-parser');
+debug = require('debug')('jam!');
+
 
 app = express();
 server = http.createServer(app);
 io = socketio(server);
 port = process.env.PORT || 3030;
 
-// express static server
-app.use(express.static(path.join(__dirname , 'static')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// setup routes
-app.get('/', function(req, res) {
-  res.json({});
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    // respond with html page
+    if (req.accepts('html')) {
+        /*TODO: handle 404 errors here */
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
 });
+
+/// error handlers
+if(app.get('env') === 'production') {
+    /*TODO: fix this for launch */
+    /*
+    try {
+        var pid = npid.create('/var/run/pmfat.pid');
+        pid.removeOnExit();
+    } catch (err) {
+        console.log('>> app.js - npmid error : ',err);
+        process.exit(1);
+    }
+    */
+}
+
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
 // socket.io setup
 io.on('connection', function(socket) {
@@ -41,11 +92,4 @@ io.on('connection', function(socket) {
   	});
 
 });
-
-// http server setup
-server.listen(port, function() {
-	console.log('&#9836; Jam! &#9836;', port);
-});
-
-
 
