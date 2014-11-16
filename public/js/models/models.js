d13711caf65f880f4fd31078ce5fbb2ca7b77c46
maniart <mani.art@gmail.com>
2014-11-15 21:45:14 -0500

    leaving it here for the night

--- public/js/models/models.js
+++ public/js/models/models.js
@@ -4,8 +4,31 @@ app.models = (function(w, d, Backbone, _, $) {
 
 	'use strict';
 
-	return {
+	var Synth;
 
+	Synth = Backbone.Model.extend({
+
+		initialize : function() {
+			console.log('models.js >> Synth : new instance');
+		},
+		urlRoot : '/instruments/synth',
+		
+		defualts : {
+			notes : {
+				'C' : 261.63,
+				'D' : 293.66,
+				'E' : 329.63,
+				'F' : 349.23,
+				'G' : 392.00,
+				'A' : 440.00,
+				'B' : 493.88 	
+			}
+		},
+
+	});
+
+	return {
+		Synth : Synth
 	};
 
 }(window, document, Backbone, _, jQuery));
