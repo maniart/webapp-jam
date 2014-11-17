968c9eb9885409454a0ff76723bbda80a9aeaa79
maniart <mani.art@gmail.com>
2014-11-16 22:56:46 -0500

    binding basic hammer events

--- public/js/views/views.js
+++ public/js/views/views.js
@@ -4,19 +4,23 @@ app.views = (function(w, d, Backbone, _, $) {
 
 	'use strict';
 
-	var Synth;
-
-	Synth = Backbone.View.extend({
+	
+	var Synth = Backbone.View.extend({
 
 		attributes : {
 			'class' : 'instrument synth col-xs-12'
 		},
+		
 		template : _.template('<div class="row"><% _.each(notes, function(note, objectKey) { %> <div class="key note" style="width:<%= 100/_.size(notes) %>%; background-color: rgba(0, 0, 0, <%= .7 / (_.keys(notes).indexOf(objectKey) + 1) %>)" data-frequency="<%= note %>"></div> <% }); %></div>'),
+		
 		$container : $('.instrument-container'),
+		
 		render : function() {
 			this.$el.
 				html(this.template({ notes : this.model.get('notes') })).
 				appendTo('.instrument-container');
+			this.$el.hammer();
+			
 		},
 
 		attachListeners : function() {
@@ -27,7 +31,18 @@ app.views = (function(w, d, Backbone, _, $) {
 		initialize : function() {
 			console.log('views.js >> Synth : new instance ');
 			this.attachListeners();
+		},
+
+		events : {
+			'tap' : function(ev) {
+				console.log('*tap ', $(ev.gesture.target).data('frequency'));
+			},
+			'pan' : function(ev) {
+				console.log('*pan ', $(ev.gesture.target).data('frequency'));
+			}
 		}
+
+
 
 	});
 
